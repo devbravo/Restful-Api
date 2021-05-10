9135ba8c806e48d8a1e361051e8aac0ee7c80ff2
Diego <diegofranco711@gmail.com>
2021-05-10 13:45:22 -0300

    building restful api

---
+++ index.js
@@ -0,0 +1,29 @@
+// Dependencies
+const http = require('http');
+const url = require('url');
+
+// The server should respond to all requests with a string
+var server = http.createServer((req, res) => {
+  // Get the URL and parse it
+  const parsedUrl = new URL(req.url, 'https://example.org/');
+
+  // Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Get the HTTP Method
+  const method = req.method.toLowerCase();
+
+  // Send the response
+  res.end('Hello World\n');
+
+  // Log the request path
+  console.log(
+    `Request received on path: ${trimmedPath} with method: ${method}`
+  );
+});
+
+// Start the server, and have it listen on port 3000
+server.listen(3000, () => {
+  console.log('The server is listening on port 3000 now');
+});
