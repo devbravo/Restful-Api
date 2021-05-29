f728f7ed2c6e2c70438420add2a43d511194a8a7
Diego <diegofranco711@gmail.com>
2021-05-29 20:39:53 -0300

    Connected api to twilio api and done some refractoring

--- index.js
+++ index.js
@@ -1,100 +1,24 @@
+/*
+ * Primary file for the API
+ */
+
 // Dependencies
-const http = require('http');
-const https = require('https');
-const { URL } = require('url');
-const StringDecoder = require('string_decoder').StringDecoder;
-const config = require('./lib/config');
-const fs = require('fs');
-const handlers = require('./lib/handlers');
-const helpers = require('./lib/helpers');
+const server = require('./lib/server');
+const workers = require('./lib/workers');
 
-// Instantiate the HTTP server
-const httpServer = http.createServer((req, res) => {
-  unifiedServer(req, res);
-});
+// Declare the app
+const app = {};
 
-// Start the server
-httpServer.listen(config.httpPort, () => {
-  console.log(`The server is listening on port ${config.httpPort}`);
-});
-
-// Instantiate the HTTPS server
-const httpsServerOptions = {
-  key: fs.readFileSync('./https/key.pem'),
-  cert: fs.readFileSync('./https/cert.pem'),
-};
-const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
-  unifiedServer(req, res);
-});
-
-// Start the HTTPS server
-httpsServer.listen(config.httpsPort, () => {
-  console.log(`The server is listening on port ${config.httpsPort}`);
-});
-
-// All the sever logic for both the http and https sever
-const unifiedServer = (req, res) => {
-  // Get the URL and parse it
-  const parsedUrl = new URL(req.url, 'http://localhost:3000/');
-
-  // Get the path and trim it using regex, get the query string as an object
-  const { pathname, searchParams } = parsedUrl;
-  const trimmedPath = pathname.replace(/^\/+|\/+$/g, '');
-  const queryStringObject = searchParams;
-
-  // Get the HTTP Method nad headers
-  const { method, headers } = req;
-
-  // Get the payload, if any
-  const decoder = new StringDecoder('utf-8');
-  let buffer = '';
-
-  req.on('data', data => {
-    buffer += decoder.write(data);
-  });
-
-  req.on('end', () => {
-    buffer += decoder.end();
-
-    // Choose the handler this request should go to. If one is not found, use the not found handler
-    const chosenHandler =
-      typeof router[trimmedPath] !== 'undefined'
-        ? router[trimmedPath]
-        : handlers.notFound;
-
-    // Construct the data object to send to the handler
-    const data = {
-      trimmedPath: trimmedPath,
-      queryStringObject: queryStringObject,
-      method: method.toLowerCase(),
-      headers: headers,
-      payload: helpers.parseJsonToObject(buffer),
-    };
-
-    // Route the request to the handler specified in the router
-    chosenHandler(data, (statusCode, payload) => {
-      // Use the status code called back by the handler or default to 200
-      statusCode = typeof statusCode == 'number' ? statusCode : 200;
-
-      // use the payload called back by the handler, or default to an empty object
-      payload = typeof payload == 'object' ? payload : {};
-
-      // Convert the payload to a string
-      const payloadStrings = JSON.stringify(payload);
-
-      // Return the response
-      res.setHeader('Content-Type', 'application/json');
-      res.writeHead(statusCode);
-      res.end(payloadStrings);
-      console.log('Returning this response:', statusCode, payloadStrings);
-    });
-  });
+// Init function
+app.init = function () {
+  // Start the server
+  server.init();
+  // Start the workers
+  workers.init();
 };
 
-// Define a request router
-const router = {
-  ping: handlers.ping,
-  users: handlers.users,
-  tokens: handlers.tokens,
-  checks: handlers.checks,
-};
+// Execute
+app.init();
+
+// Export the app
+module.exports = app;
